4fc4450440bd652e359196776897015ac46751de
Amulya Shetty <79209035+amulyaashetty@users.noreply.github.com>
2022-06-23 18:46:26 +0530

    Add files via upload

---
+++ covid help/style.js
@@ -0,0 +1,10 @@
+<div class="cards">
+      <div class="card">
+          <h1 class="project-tile">Covid 19 Charts</h1>
+     <marquee width="100%" direction="up" height="150px" scrollamount="2">
+          <img src="https://images.moneycontrol.com/static-mcnews/2020/12/covid-19-Dec-3_update.png"height="300px" width="300px">
+        <img src="https://images.moneycontrol.com/static-mcnews/2020/12/covid-19-Dec-3_update.png"height="300px" width="300px">
+        <img src="https://images.moneycontrol.com/static-mcnews/2020/12/covid-19-Dec-3_update.png"height="300px" width="300px">
+      </div>
+     </marquee>
+          
